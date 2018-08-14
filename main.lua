35254ef107352e692c5eb29cec87d06575ec8ff3
draxxthemsklounst <37089851+draxxthemsklounst@users.noreply.github.com>
2018-08-14 14:48:29 +1000

    sprite
    added megaman sprite

--- main.lua
+++ main.lua
@@ -9,8 +9,10 @@ function love.load()
 		resizable = false,
 		vsync = true } )
 
+  love.graphics.setDefaultFilter('nearest','nearest')
+  
+  
   gravityConstant = 0.5; 
-
   floorY = 560
 
   player = {}
@@ -20,13 +22,15 @@ function love.load()
   player.yVelocity = 0
   player.width = 30
   player.height = 30
-  
-  
+  player.rightSprite = love.graphics.newImage('megamanRIGHT.png')
+  player.leftSprite = love.graphics.newImage('megamanLEFT.png')
   player.jump = function()
     if player.y == floorY then
       player.yVelocity = -8
     end
   end
+  
+  
   
   
 end
@@ -42,10 +46,12 @@ end
 
 
 function love.update(dt)
+  --END CHECKER
   if gameOver ~= false then
     love.draw()
   end
   
+  --KEYBOARD INPUT
   if love.keyboard.isDown("space") then
     player.jump()
   end
@@ -55,15 +61,15 @@ elseif love.keyboard.isDown("a") then
     player.xVelocity = -5
   end
   
+  --PHYSICS UPDATE
   player.x = player.x + player.xVelocity
   player.y = player.y + player.yVelocity
   player.yVelocity = player.yVelocity + gravityConstant
-  
   if player.y == floorY then
     player.yVelocity = 0
   end
   
-  
+  --VELOCITY RESET
   player.xVelocity = 0
 end
 
@@ -73,8 +79,14 @@ function love.draw()
   love.graphics.rectangle("fill",0,floorY ,WINDOW_WIDTH,WINDOW_HEIGHT - floorY)
   
   --Draws the Player
-  love.graphics.setColor(0,1,0)
-	love.graphics.rectangle("fill",player.x,player.y - player.height,player.width,player.height)
+  --love.graphics.setColor(0,0,0)
+  if player.xVelocity > 0 then
+	love.graphics.draw(player.rightSprite,player.x,player.y - player.height + 4,0,1)
+elseif player.xVelocity < 0 then
+  love.graphics.draw(player.leftSprite,player.x,player.y - player.height + 4,0,1)
+  end
+  love.graphics.rectangle("line",player.x,player.y - player.height,player.width,player.height)
+
   
   
   
